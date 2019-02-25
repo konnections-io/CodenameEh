7c76c8e9a6ccb62b465f8c9cb13eb65b92d594fe
Ryan Jensen <rrjensen@ualberta.ca>
2019-02-24 22:45:55 -0700

    Finished User test cases, remove the random changes when merging (build.gradle, misc.xml and so forth)

---
+++ app/src/test/java/com/example/codenameeh/KeywordUnitTest.java
@@ -0,0 +1,7 @@
+package com.example.codenameeh;
+
+public class KeywordUnitTest {
+    // DATABASE TESTS MUST BE DONE MANUALLY, as I don't know how to assert that
+    // This instead only checks adding, removing should perhaps also be added...
+
+}
