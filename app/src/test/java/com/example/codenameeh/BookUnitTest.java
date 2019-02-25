9a6786b4bf11c2abbd580556fad72b32515c0b15
Cole Boytinck <cboytinc@ualberta.ca>
2019-02-24 21:30:54 -0700

    Edited book unit test to match string ISBN

--- app/src/test/java/com/example/codenameeh/BookUnitTest.java
+++ app/src/test/java/com/example/codenameeh/BookUnitTest.java
@@ -2,8 +2,10 @@ package com.example.codenameeh;
 
 import org.junit.Test;
 
+import classes.Book;
 import classes.UserValidifier;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
@@ -11,7 +13,7 @@ public class BookUnitTest{
       @Test
       public void test(){
          //Test get/set title, get/set author, get/set ISBN, get/set description, get/set photograph, isBorrowed, setBorrowed (boolean)
-         Book book = new Book("NA","NA",0,"NA","NA");
+         Book book = new Book("NA","NA","0","NA","NA");
          
          book.setTitle("Original Title");
          assertEquals(book.getTitle(),"Original Title");
@@ -19,8 +21,8 @@ public class BookUnitTest{
          book.setAuthor("An Author");
          assertEquals(book.getAuthor(),"An Author");
          
-         book.setISBN(123123123);
-         assertEquals(book.getISBN(),123123123);
+         book.setISBN("123123123");
+         assertEquals(book.getISBN(),"123123123");
          
          book.setDescription("Animals");
          assertEquals(book.getDescription(),"Animals");
@@ -28,9 +30,9 @@ public class BookUnitTest{
          book.setPhotograph("Cat.png");
          assertEquals(book.getPhotograph(),"Cat.png");
          
-         assertFalse(book.isBorrowed);
-         book.setBorrowed(True);
-         assertTrue(book.isBorrowed);
+         assertFalse(book.isBorrowed());
+         book.setBorrowed(true);
+         assertTrue(book.isBorrowed());
          
       
       
