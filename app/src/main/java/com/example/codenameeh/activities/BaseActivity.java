6ff39c2c81eb79f6c8482620a5613f8a09beb562
Cole Boytinck <cboytinc@ualberta.ca>
2019-03-07 11:07:00 -0700

    Changed sidebar actions

--- app/src/main/java/com/example/codenameeh/activities/BaseActivity.java
+++ app/src/main/java/com/example/codenameeh/activities/BaseActivity.java
@@ -70,17 +70,15 @@ public class BaseActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_my_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_notifications) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_scan) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_search_books) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
