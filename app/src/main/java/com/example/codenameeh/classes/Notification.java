0c278ac1a92b40aa25d2ad7c339770f4931dc29a
bqi1 <brian.qi138@gmail.com>
2019-03-09 22:00:17 -0700

    Declared AcceptedBookNotification and RequestNotification classes. Changed Notification class to include two types of notifications.

--- app/src/main/java/com/example/codenameeh/classes/Notification.java
+++ app/src/main/java/com/example/codenameeh/classes/Notification.java
@@ -5,21 +5,26 @@ import java.util.ArrayList;
 public class Notification {
 
     private User user;
-    private ArrayList<String> notifications;
 
     public Notification(User user) {
         this.user = user;
-        this.notifications = new ArrayList<String>();
     }
 
-    public void add(String notification) {
-       notifications.add(notification);
+    public void add(String userWanting, String bookWanted) {
+        //Use user to access and add specified notification
     }
 
-    public void remove(String notification) {
-        notifications.remove(notification);
+    public void add(String userGiving, String bookGiven, String geolocation) {
+        //Use user to access and add specified notification
     }
 
+    public void remove(String userWanting, String bookWanted) {
+        //Use user to access and remove specific notification
+    }
+
+    public void remove(String userGiving, String bookGiven, String geolocation) {
+        //Use user to access and remove specific notification
+    }
     public User getUser() {
         return user;
     }
@@ -28,11 +33,13 @@ public class Notification {
         this.user = user;
     }
 
-    public ArrayList<String> getNotifications() {
-        return notifications;
-    }
+    public void getRequestNotifications() {
+        //Call database for retrieval of tuple of 2 strings, user wanting, and book wanted
 
-    public void setNotifications(ArrayList<String> notifications) {
-        this.notifications = notifications;
-    }
+        }
+
+    public void getAcceptedBookNotifications() {
+        //Call database for retrieval of tuple of 3 strings, user giving, book being given, geolocation
+
+        }
 }
