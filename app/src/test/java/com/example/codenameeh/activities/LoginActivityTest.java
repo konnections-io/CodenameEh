adf4f0402362fb47aa67d486215ffd0afa8fe933
Cole Boytinck <cboytinc@ualberta.ca>
2019-03-11 11:46:47 -0600

    Added outline for test cases

---
+++ app/src/test/java/com/example/codenameeh/activities/LoginActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.codenameeh.activities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+
+    @Test
+    public void onStart() {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+}
---
+++ app/src/test/java/com/example/codenameeh/activities/ProfileActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.codenameeh.activities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProfileActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onRestart() {
+    }
+}
